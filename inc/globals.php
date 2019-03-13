2760dbf2d838557e3cc89b9867873bafee851ef3
Victor Ivanov <victor@maltcreative.com.au>
2019-03-13 11:22:43 +1100

    added globals.php

---
+++ inc/globals.php
@@ -0,0 +1,11 @@
+<?php
+
+$colors = array(
+  'transparent' => 'transparent',
+  'white' 			=> '#FFFFFF',
+  'black'				=> '#000000',
+  'grey'				=> '#F2F2F2',
+  'dark_grey'		=> '#4A4A4A',
+  'brand_red'		=> '#EB1D24',
+  'transparent'	=> 'transparent'
+);

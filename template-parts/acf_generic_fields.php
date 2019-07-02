73ac64a2533157f582c0eec2b1fe3844e53dfd80
Victor Ivanov <reachvctr@gmail.com>
2019-07-02 01:22:17 +0000

    Cater for both sub_fields (flex children) and standard flat fields layout

--- template-parts/acf_generic_fields.php
+++ template-parts/acf_generic_fields.php
@@ -1,10 +1,16 @@
 <?php
 
-$advanced_visible = get_sub_field( 'show_advanced_settings' );
+$advanced_visible_sub = get_sub_field( 'show_advanced_settings' );
+$advanced_visible = get_field( 'show_advanced_settings' );
 
-if ( $advanced_visible ) {
-  $adv = get_sub_field( 'advanced_settings' );
-  $padding = ( $adv[ 'show_padding_options' ] ) ? $adv[ 'padding' ] : null;
-  $custom_classes = ( $adv[ 'add_custom_classes' ] ) ? $adv[ 'custom_classes' ] : null;
-  $bg = ( $adv[ 'show_bg_options' ] ) ? $adv[ 'bg' ] : null;
+if ( $advanced_visible_sub ) {
+	$adv = get_sub_field( 'advanced_settings' );
+	$padding = ( $adv[ 'show_padding_options' ] ) ? $adv[ 'padding' ] : null;
+	$custom_classes = ( $adv[ 'add_custom_classes' ] ) ? $adv[ 'custom_classes' ] : null;
+	$bg = ( $adv[ 'show_bg_options' ] ) ? $adv[ 'bg' ] : null;
+} else if ( $advanced_visible ) {
+	$adv = get_field( 'advanced_settings' );
+	$padding = ( $adv[ 'show_padding_options' ] ) ? $adv[ 'padding' ] : null;
+	$custom_classes = ( $adv[ 'add_custom_classes' ] ) ? $adv[ 'custom_classes' ] : null;
+	$bg = ( $adv[ 'show_bg_options' ] ) ? $adv[ 'bg' ] : null;
 }
